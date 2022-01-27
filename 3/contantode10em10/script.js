acf2f0076fe5f10a35f27c41cae3241fe5145231
Leticiafrnc <leticiafrnc1@gmail.com>
2022-01-27 16:36:56 -0300

    update atividade 3ano

--- 3/contantode10em10/script.js
+++ 3/contantode10em10/script.js
@@ -1,162 +1,58 @@
-function conferirPolvo() {
-    const respPolvo = document.querySelector('input#respostaPolvo')
-    const resPolvo = document.querySelector('div#resultadoPolvo')
-    const resultadoPolvo = String(respPolvo.value)
+function conferirResposta40() {
+    const respResposta40 = document.querySelector('input#resposta40')
+    const resResposta40 = document.querySelector('div#resultado40')
+    const resultadoResposta40 = String(respResposta40.value)
     
-    if (resultadoPolvo === 'Polvo') {
-        resPolvo.innerHTML = ` Parabéns, está correto!` 
+    if (resultadoResposta40 === '40') {
+        resResposta40.innerHTML = ` Parabéns a resposta 40, está correto! ` 
     } else{
-        resPolvo.innerHTML = `Resposta errada, tente outra vez!`
+        resResposta40.innerHTML = `Resposta do 1º quadrado esta errada, tente outra vez!`
     }
     } 
 
-    function conferirJornal() {
-        const respJornal = document.querySelector('input#respostaJornal')
-        const resJornal = document.querySelector('div#resultadoJornal')
-        const resultadoJornal = String(respJornal.value)
-
-        if (resultadoJornal === `Jornal`) {
-            resJornal.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resJornal.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-
-    function conferirMingau () {
-        const respMingau = document.querySelector('input#respostaMingau')
-        const resMingau = document.querySelector('div#resultadoMingau')
-        const resultadoMingau = String(respMingau.value)
+function conferirResposta50() {
+        const respResposta50 = document.querySelector('input#resposta50')
+        const resResposta50 = document.querySelector('div#resultado50')
+        const resultadoResposta50 = String(respResposta50.value)
         
-        if (resultadoMingau === `Mingau`) {
-            resMingau.innerHTML = `Parabéns, está correto!` 
+        if (resultadoResposta50 === '50') {
+            resResposta50.innerHTML = ` Parabéns a resposta 50, está correto!` 
+        }else{
+                resResposta50.innerHTML = `Resposta do 2º quadrado esta errada, tente outra vez!`
+            }
+            
+        } 
+
+function conferirResposta60() {
+            const respResposta60 = document.querySelector('input#resposta60')
+            const resResposta60 = document.querySelector('div#resultado60')
+            const resultadoResposta60 = String(respResposta60.value)
+            
+            if (resultadoResposta60 === '60') {
+                resResposta60.innerHTML = ` Parabéns a resposta 60, está correto!` 
+            } else{
+                resResposta60.innerHTML = `Resposta do 3º quadrado esta errada, tente outra vez!`
+            }
+            } 
+function conferirResposta70() {
+                const respResposta70 = document.querySelector('input#resposta70')
+                const resResposta70 = document.querySelector('div#resultado70')
+                const resultadoResposta70 = String(respResposta70.value)
+                
+                if (resultadoResposta70 === '70') {
+                    resResposta70.innerHTML = ` Parabéns a resposta 70, está correto!` 
+                } else{
+                    resResposta70.innerHTML = `Resposta do 4º quadrado esta errada, tente outra vez!`
+                }
+                } 
+function conferirResposta80() {
+    const respResposta80 = document.querySelector('input#resposta80')
+    const resResposta80 = document.querySelector('div#resultado80')
+    const resultadoResposta80 = String(respResposta80.value)
+                    
+    if (resultadoResposta80 === '80') {
+        resResposta80.innerHTML = ` Parabéns a resposta 80, está correto!` 
         } else{
-            resMingau.innerHTML = `Resposta errada, tente outra vez!`
+            resResposta80.innerHTML = `Resposta do 5º quadrado esta errada, tente outra vez!!`
         }
-    }
-    function conferirAlgodao() {
-        const respAlgodao = document.querySelector('input#respostaAlgodao')
-        const resAlgodao = document.querySelector('div#resultadoAlgodao')
-        const resultadoAlgodao = String(respAlgodao.value)
-
-        if (resultadoAlgodao === `Algodão`) {
-            resAlgodao.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resAlgodao.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirDegrau(){
-        const respDegrau = document.querySelector('input#respostaDegrau')
-        const resDegrau = document.querySelector('div#resultadoDegrau')
-        const resultadoDegrau = String(respDegrau.value)
-
-        if (resultadoDegrau === `Degrau`) {
-            resDegrau.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resDegrau.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirAlface() {
-        const respAlface = document.querySelector('input#respostaAlface')
-        const resAlface = document.querySelector('div#resultadoAlface')
-        const resultadoAlface = String(respAlface.value)
-
-        if (resultadoAlface === `Alface`) {
-            resAlface.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resAlface.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirPincel() {
-        const respPincel = document.querySelector('input#respostaPincel')
-        const resPincel = document.querySelector('div#resultadoPincel')
-        const resultadoPincel = String(respPincel.value)
-
-        if (resultadoPincel === `Pincel`) {
-            resPincel.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resPincel.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirPauta() {
-        const respPauta = document.querySelector('input#respostaPauta')
-        const resPauta = document.querySelector('div#resultadoPauta')
-        const resultadoPauta = String(respPauta.value)
-
-        if (resultadoPauta === `Pauta`) {
-            resPauta.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resPauta.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirGrau() {
-        const respGrau = document.querySelector('input#respostaGrau')
-        const resGrau = document.querySelector('div#resultadoGrau')
-        const resultadoGrau = String(respGrau.value)
-
-        if (resultadoGrau === `Grau`) {
-            resGrau.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resGrau.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirCafezal() {
-        const respCafezal = document.querySelector('input#respostaCafezal')
-        const resCafezal = document.querySelector('div#resultadoCafezal')
-        const resultadoCafezal = String(respCafezal.value)
-
-        if (resultadoCafezal === `Cafezal`) {
-            resCafezal.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resCafezal.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirPalmito() {
-        const respPalmito = document.querySelector('input#respostaPalmito')
-        const resPalmito = document.querySelector('div#resultadoPalmito')
-        const resultadoPalmito = String(respPalmito.value)
-
-        if (resultadoPalmito === `Palmito`) {
-            resPalmito.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resPalmito.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirChapeu() {
-        const respChapeu = document.querySelector('input#respostaChapeu')
-        const resChapeu = document.querySelector('div#resultadoChapeu')
-        const resultadoChapeu = String(respChapeu.value)
-
-        if (resultadoChapeu === `Chapéu`) {
-            resChapeu.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resChapeu.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    
-    function conferirNormal() {
-        const respNormal = document.querySelector('input#respostaNormal')
-        const resNormal = document.querySelector('div#resultadoNormal')
-        const resultadoNormal = String(respNormal.value)
-
-        if (resultadoNormal === `Normal`) {
-            resNormal.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resNormal.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-    function conferirQuintal() {
-        const respQuintal = document.querySelector('input#respostaQuintal')
-        const resQuintal = document.querySelector('div#resultadoQuintal')
-        const resultadoQuintal = String(respQuintal.value)
-
-        if (resultadoQuintal === `Quintal`) {
-            resQuintal.innerHTML = `Parabéns, está correto!` 
-        } else{
-            resQuintal.innerHTML = `Resposta errada, tente outra vez!`
-        }
-    }
-
-   
-    
-
-   
+    } 
