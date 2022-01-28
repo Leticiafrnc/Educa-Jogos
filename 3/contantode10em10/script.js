2943b3f6a604c6e62f15c6e940ae8385e8a342d6
Leticiafrnc <leticiafrnc1@gmail.com>
2022-01-28 09:05:40 -0300

    add script  atividade 3 ano

--- 3/contantode10em10/script.js
+++ 3/contantode10em10/script.js
@@ -2,57 +2,115 @@ function conferirResposta40() {
     const respResposta40 = document.querySelector('input#resposta40')
     const resResposta40 = document.querySelector('div#resultado40')
     const resultadoResposta40 = String(respResposta40.value)
-    
+
     if (resultadoResposta40 === '40') {
-        resResposta40.innerHTML = ` Parabéns a resposta 40, está correto! ` 
-    } else{
+        resResposta40.innerHTML = ` Parabéns a resposta 40, está correto! `
+    } else {
         resResposta40.innerHTML = `Resposta do 1º quadrado esta errada, tente outra vez!`
     }
-    } 
+}
 
 function conferirResposta50() {
-        const respResposta50 = document.querySelector('input#resposta50')
-        const resResposta50 = document.querySelector('div#resultado50')
-        const resultadoResposta50 = String(respResposta50.value)
-        
-        if (resultadoResposta50 === '50') {
-            resResposta50.innerHTML = ` Parabéns a resposta 50, está correto!` 
-        }else{
-                resResposta50.innerHTML = `Resposta do 2º quadrado esta errada, tente outra vez!`
-            }
-            
-        } 
+    const respResposta50 = document.querySelector('input#resposta50')
+    const resResposta50 = document.querySelector('div#resultado50')
+    const resultadoResposta50 = String(respResposta50.value)
+
+    if (resultadoResposta50 === '50') {
+        resResposta50.innerHTML = ` Parabéns a resposta 50, está correto!`
+    } else {
+        resResposta50.innerHTML = `Resposta do 2º quadrado esta errada, tente outra vez!`
+    }
+
+}
 
 function conferirResposta60() {
-            const respResposta60 = document.querySelector('input#resposta60')
-            const resResposta60 = document.querySelector('div#resultado60')
-            const resultadoResposta60 = String(respResposta60.value)
-            
-            if (resultadoResposta60 === '60') {
-                resResposta60.innerHTML = ` Parabéns a resposta 60, está correto!` 
-            } else{
-                resResposta60.innerHTML = `Resposta do 3º quadrado esta errada, tente outra vez!`
-            }
-            } 
+    const respResposta60 = document.querySelector('input#resposta60')
+    const resResposta60 = document.querySelector('div#resultado60')
+    const resultadoResposta60 = String(respResposta60.value)
+
+    if (resultadoResposta60 === '60') {
+        resResposta60.innerHTML = ` Parabéns a resposta 60, está correto!`
+    } else {
+        resResposta60.innerHTML = `Resposta do 3º quadrado esta errada, tente outra vez!`
+    }
+}
 function conferirResposta70() {
-                const respResposta70 = document.querySelector('input#resposta70')
-                const resResposta70 = document.querySelector('div#resultado70')
-                const resultadoResposta70 = String(respResposta70.value)
-                
-                if (resultadoResposta70 === '70') {
-                    resResposta70.innerHTML = ` Parabéns a resposta 70, está correto!` 
-                } else{
-                    resResposta70.innerHTML = `Resposta do 4º quadrado esta errada, tente outra vez!`
-                }
-                } 
+    const respResposta70 = document.querySelector('input#resposta70')
+    const resResposta70 = document.querySelector('div#resultado70')
+    const resultadoResposta70 = String(respResposta70.value)
+
+    if (resultadoResposta70 === '70') {
+        resResposta70.innerHTML = ` Parabéns a resposta 70, está correto!`
+    } else {
+        resResposta70.innerHTML = `Resposta do 4º quadrado esta errada, tente outra vez!`
+    }
+}
 function conferirResposta80() {
     const respResposta80 = document.querySelector('input#resposta80')
     const resResposta80 = document.querySelector('div#resultado80')
     const resultadoResposta80 = String(respResposta80.value)
-                    
+
     if (resultadoResposta80 === '80') {
-        resResposta80.innerHTML = ` Parabéns a resposta 80, está correto!` 
-        } else{
-            resResposta80.innerHTML = `Resposta do 5º quadrado esta errada, tente outra vez!!`
-        }
-    } 
+        resResposta80.innerHTML = ` Parabéns a resposta 80, está correto!`
+    } else {
+        resResposta80.innerHTML = `Resposta do 5º quadrado esta errada, tente outra vez!!`
+    }
+} 
+function conferirResposta90() {
+    const respResposta90 = document.querySelector('input#resposta90')
+    const resResposta90 = document.querySelector('div#resultado90')
+    const resultadoResposta90 = String(respResposta90.value)
+
+    if (resultadoResposta90 === '90') {
+        resResposta90.innerHTML = ` Parabéns a resposta 90, está correto!`
+    } else {
+        resResposta90.innerHTML = `Resposta do 6º quadrado esta errada, tente outra vez!!`
+    }
+} 
+function conferirResposta100() {
+    const respResposta100 = document.querySelector('input#resposta100')
+    const resResposta100 = document.querySelector('div#resultado100')
+    const resultadoResposta100 = String(respResposta100.value)
+
+    if (resultadoResposta100 === '100') {
+        resResposta100.innerHTML = ` Parabéns a resposta 100, está correto!`
+    } else {
+        resResposta100.innerHTML = `Resposta do 7º quadrado esta errada, tente outra vez!!`
+    }
+} 
+function conferirResposta200() {
+    const respResposta200 = document.querySelector('input#resposta200')
+    const resResposta200 = document.querySelector('div#resultado200')
+    const resultadoResposta200 = String(respResposta200.value)
+
+    if (resultadoResposta200 === '200') {
+        resResposta200.innerHTML = ` Parabéns a resposta 200, está correto!`
+    } else {
+        resResposta200.innerHTML = `Resposta do 8º quadrado esta errada, tente outra vez!!`
+    }
+} 
+function conferirResposta300() {
+    const respResposta300 = document.querySelector('input#resposta300')
+    const resResposta300 = document.querySelector('div#resultado300')
+    const resultadoResposta300 = String(respResposta300.value)
+
+    if (resultadoResposta300 === '300') {
+        resResposta300.innerHTML = ` Parabéns a resposta 300, está correto!`
+    } else {
+        resResposta300.innerHTML = `Resposta do 9º quadrado esta errada, tente outra vez!!`
+    }
+} 
+function conferirResposta400() {
+    const respResposta400 = document.querySelector('input#resposta400')
+    const resResposta400 = document.querySelector('div#resultado400')
+    const resultadoResposta400 = String(respResposta400.value)
+
+    if (resultadoResposta400 === '400') {
+        resResposta400.innerHTML = ` Parabéns a resposta 400, está correto!`
+    } else {
+        resResposta400.innerHTML = `Resposta do 10º quadrado esta errada, tente outra vez!!`
+    }
+} 
+
+
+
