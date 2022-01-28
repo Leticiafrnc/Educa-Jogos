19932b6b666442086d7b775e3d3d9a863b6cd092
Leticiafrnc <leticiafrnc1@gmail.com>
2022-01-28 15:06:27 -0300

    update

---
+++ 5/jogocousc/script.js
@@ -0,0 +1,128 @@
+function conferirPiscina() {
+    const respPiscina = document.querySelector('input#respostaPiscina')
+    const resPiscina = document.querySelector('div#resultadoPiscina')
+    const resultadoPiscina = String(respPiscina.value)
+    
+    if (resultadoPiscina === 'Piscina') {
+        resPiscina.innerHTML = ` Parabéns, está correto!` 
+    } else{
+        resPiscina.innerHTML = `Resposta errada, tente outra vez!`
+    }
+    } 
+
+    function conferirCinema() {
+        const respCinema = document.querySelector('input#respostaCinema')
+        const resCinema = document.querySelector('div#resultadoCinema')
+        const resultadoCinema = String(respCinema.value)
+
+        if (resultadoCinema === `Cinema`) {
+            resCinema.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resCinema.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+
+    function conferirCrescer() {
+        const respCrescer = document.querySelector('input#respostaCrescer')
+        const resCrescer = document.querySelector('div#resultadoCrescer')
+        const resultadoCrescer = String(respCrescer.value)
+
+        if (resultadoCrescer === `Crescer`) {
+            resCrescer.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resCrescer.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+    function conferirCimento() {
+        const respCimento = document.querySelector('input#respostaCimento')
+        const resCimento = document.querySelector('div#resultadoCimento')
+        const resultadoCimento = String(respCimento.value)
+
+        if (resultadoCimento === `Cimento`) {
+            resCimento.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resCimento.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+    function conferirCerto(){
+        const respCerto = document.querySelector('input#respostaCerto')
+        const resCerto = document.querySelector('div#resultadoCerto')
+        const resultadoCerto = String(respCerto.value)
+
+        if (resultadoCerto === `Certo`) {
+            resCerto.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resCerto.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+    function conferirPescoco() {
+        const respPescoco = document.querySelector('input#respostaPescoco')
+        const resPescoco = document.querySelector('div#resultadoPescoco')
+        const resultadoPescoco = String(respPescoco.value)
+
+        if (resultadoPescoco === `Pescoço`) {
+            resPescoco.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resPescoco.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+    function conferirAcima() {
+        const respAcima = document.querySelector('input#respostaAcima')
+        const resAcima = document.querySelector('div#resultadoAcima')
+        const resultadoAcima = String(respAcima.value)
+
+        if (resultadoAcima === `Acima`) {
+            resAcima.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resAcima.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+    function conferirTecido() {
+        const respTecido = document.querySelector('input#respostaTecido')
+        const resTecido = document.querySelector('div#resultadoTecido')
+        const resultadoTecido = String(respTecido.value)
+
+        if (resultadoTecido === `Tecido`) {
+            resTecido.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resTecido.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+    function conferirNascer() {
+        const respNascer = document.querySelector('input#respostaNascer')
+        const resNascer = document.querySelector('div#resultadoNascer')
+        const resultadoNascer = String(respNascer.value)
+
+        if (resultadoNascer === `Nascer`) {
+            resNascer.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resNascer.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+    function conferirDescer() {
+        const respDescer = document.querySelector('input#respostaDescer')
+        const resDescer = document.querySelector('div#resultadoDescer')
+        const resultadoDescer = String(respDescer.value)
+
+        if (resultadoDescer === `Descer`) {
+            resDescer.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resDescer.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+    function conferirNascente() {
+        const respNascente = document.querySelector('input#respostaNascente')
+        const resNascente = document.querySelector('div#resultadoNascente')
+        const resultadoNascente = String(respNascente.value)
+
+        if (resultadoNascente === `Nascente`) {
+            resNascente.innerHTML = `Parabéns, está correto!` 
+        } else{
+            resNascente.innerHTML = `Resposta errada, tente outra vez!`
+        }
+    }
+
+   
+    
+
+   
