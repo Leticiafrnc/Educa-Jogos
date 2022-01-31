cc8726286156831a0f99f27e551d5d6c73850a5f
Leticiafrnc <leticiafrnc1@gmail.com>
2022-01-31 16:36:20 -0300

    update

---
+++ 1/letra-inicial/app.js
@@ -0,0 +1,82 @@
+function conferirRato() {
+  const respA = document.getElementById("respA").value;
+  if (respA === 'R') 
+  {
+    document.getElementById("respRato").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("respRato").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirBala() {
+  const respBala = document.getElementById("respBala").value;
+  if (respBala === 'B') 
+  {
+    document.getElementById("resBala").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resBala").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+
+function conferirBolo() {
+  const respBolo = document.getElementById("respBolo").value;
+  if (respBolo === 'B') 
+  {
+    document.getElementById("resBolo").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resBolo").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirBule() {
+  const respBule = document.getElementById("respBule").value;
+  if (respBule === 'B') 
+  {
+    document.getElementById("resBule").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resBule").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirFogo() {
+  const respFogo = document.getElementById("respFogo").value;
+  if (respFogo === 'F') 
+  {
+    document.getElementById("resFogo").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resFogo").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirMala() {
+  const respMala = document.getElementById("respMala").value;
+  if (respMala === 'M') 
+  {
+    document.getElementById("resMala").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resMala").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirRede() {
+  const respRede = document.getElementById("respRede").value;
+  if (respRede === 'R') 
+  {
+    document.getElementById("resRede").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resRede").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirPato() {
+  const respPato = document.getElementById("respPato").value;
+  if (respPato === 'P') 
+  {
+    document.getElementById("resPato").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resPato").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
