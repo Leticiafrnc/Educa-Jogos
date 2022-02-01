a49b5cccce2b549271537be64b9d9c3d0900bcca
Leticiafrnc <leticiafrnc1@gmail.com>
2022-02-01 10:43:52 -0300

    atividade 1 ano

---
+++ 1/somadasestrelas/app.js
@@ -0,0 +1,40 @@
+function conferirSoma1() {
+  const respSoma1 = document.getElementById("respSoma1").value;
+  if (respSoma1 === '4') 
+  {
+    document.getElementById("resSoma1").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resSoma1").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirSoma2() {
+  const respSoma2 = document.getElementById("respSoma2").value;
+  if (respSoma2 === '2') 
+  {
+    document.getElementById("resSoma2").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resSoma2").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirSoma3() {
+  const respSoma3 = document.getElementById("respSoma3").value;
+  if (respSoma3 === '3') 
+  {
+    document.getElementById("resSoma3").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resSoma3").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
+function conferirSoma4() {
+  const respSoma4 = document.getElementById("respSoma4").value;
+  if (respSoma4 === '5') 
+  {
+    document.getElementById("resSoma4").innerHTML = "Parabéns! Você acertou";
+  }
+  else {
+    document.getElementById("resSoma4").innerHTML = "Resposta Errada, tente novamente!";
+  }
+}
