f57be4abe0c31a4a7a5842323bbaf98446986df2
Leticiafrnc <leticiafrnc1@gmail.com>
2022-02-02 13:58:50 -0300

    atividade 2 ano

---
+++ 2/maiusculaseminusculas/app.js
@@ -0,0 +1,260 @@
+function conferirA() {
+    const respostaA = document.getElementById("respostaA").value;
+
+    if (respostaA == "a") {
+        document.getElementById("respA").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respA").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirB() {
+    const respostaB = document.getElementById("respostaB").value;
+
+    if (respostaB == "b") {
+        document.getElementById("respB").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respB").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirC() {
+    const respostaC = document.getElementById("respostaC").value;
+
+    if (respostaC == "c") {
+        document.getElementById("respC").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respC").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirD() {
+    const respostaD = document.getElementById("respostaD").value;
+
+    if (respostaD == "d") {
+        document.getElementById("respD").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respD").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirE() {
+    const respostaE = document.getElementById("respostaE").value;
+
+    if (respostaE == "e") {
+        document.getElementById("respE").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respE").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirF() {
+    const respostaF = document.getElementById("respostaF").value;
+
+    if (respostaF == "f") {
+        document.getElementById("respF").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respF").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirG() {
+    const respostaG = document.getElementById("respostaG").value;
+
+    if (respostaG == "g") {
+        document.getElementById("respG").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respG").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirH() {
+    const respostaH = document.getElementById("respostaH").value;
+
+    if (respostaH == "h") {
+        document.getElementById("respH").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respH").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirI() {
+    const respostaI = document.getElementById("respostaI").value;
+
+    if (respostaI == "i") {
+        document.getElementById("respI").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respI").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirJ() {
+    const respostaJ = document.getElementById("respostaJ").value;
+
+    if (respostaJ == "j") {
+        document.getElementById("respJ").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respJ").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirK() {
+    const respostaK = document.getElementById("respostaK").value;
+
+    if (respostaK == "k") {
+        document.getElementById("respK").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respK").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirL() {
+    const respostaL = document.getElementById("respostaL").value;
+
+    if (respostaL == "l") {
+        document.getElementById("respL").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respL").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirM() {
+    const respostaM = document.getElementById("respostaM").value;
+
+    if (respostaM == "m") {
+        document.getElementById("respM").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respM").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirN() {
+    const respostaN = document.getElementById("respostaN").value;
+
+    if (respostaN == "n") {
+        document.getElementById("respN").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respN").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirO() {
+    const respostaO = document.getElementById("respostaO").value;
+
+    if (respostaO == "o") {
+        document.getElementById("respO").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respO").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirP() {
+    const respostaP = document.getElementById("respostaP").value;
+
+    if (respostaP == "p") {
+        document.getElementById("respP").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respP").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirQ() {
+    const respostaQ = document.getElementById("respostaQ").value;
+
+    if (respostaQ == "q") {
+        document.getElementById("respQ").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respQ").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirR() {
+    const respostaR = document.getElementById("respostaR").value;
+
+    if (respostaR == "r") {
+        document.getElementById("respR").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respR").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirS() {
+    const respostaS = document.getElementById("respostaS").value;
+
+    if (respostaS == "s") {
+        document.getElementById("respS").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respS").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirT() {
+    const respostaT = document.getElementById("respostaT").value;
+
+    if (respostaT == "t") {
+        document.getElementById("respT").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respT").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirU() {
+    const respostaU = document.getElementById("respostaU").value;
+
+    if (respostaU == "u") {
+        document.getElementById("respU").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respU").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirV() {
+    const respostaV = document.getElementById("respostaV").value;
+
+    if (respostaV == "v") {
+        document.getElementById("respV").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respV").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirW() {
+    const respostaW = document.getElementById("respostaW").value;
+
+    if (respostaW == "w") {
+        document.getElementById("respW").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respW").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirX() {
+    const respostaX = document.getElementById("respostaX").value;
+
+    if (respostaX == "x") {
+        document.getElementById("respX").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respX").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirY() {
+    const respostaY = document.getElementById("respostaY").value;
+
+    if (respostaY == "y") {
+        document.getElementById("respY").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respY").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirZ() {
+    const respostaZ = document.getElementById("respostaZ").value;
+
+    if (respostaZ == "z") {
+        document.getElementById("respZ").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respZ").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
