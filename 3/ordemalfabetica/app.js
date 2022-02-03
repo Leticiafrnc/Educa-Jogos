42449a5347a5700688f6880a69cd872de1fe4028
Leticiafrnc <leticiafrnc1@gmail.com>
2022-02-03 13:49:12 -0300

    add atividade ordem alfabetica

---
+++ 3/ordemalfabetica/app.js
@@ -0,0 +1,278 @@
+function conferirArara() {
+    const respostaArara = document.getElementById("respostaArara").value;
+
+    if (respostaArara == "Arara") {
+        document.getElementById("respArara").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respArara").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirBorboleta() {
+    const respostaBorboleta = document.getElementById("respostaBorboleta").value;
+
+    if (respostaBorboleta == "Borboleta") {
+        document.getElementById("respBorboleta").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respBorboleta").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirCavalo() {
+    const respostaCavalo = document.getElementById("respostaCavalo").value;
+
+    if (respostaCavalo == "Cavalo") {
+        document.getElementById("respCavalo").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respCavalo").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirDragao() {
+    const respostaDragao = document.getElementById("respostaDragao").value;
+
+    if (respostaDragao == "Dragão") {
+        document.getElementById("respDragao").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respDragao").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirEma() {
+    const respostaEma = document.getElementById("respostaEma").value;
+
+    if (respostaEma == "Ema") {
+        document.getElementById("respEma").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respEma").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirFormiga() {
+    const respostaFormiga = document.getElementById("respostaFormiga").value;
+
+    if (respostaFormiga == "Formiga") {
+        document.getElementById("respFormiga").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respFormiga").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirGamba() {
+    const respostaGamba = document.getElementById("respostaGamba").value;
+
+    if (respostaGamba == "Gambá") {
+        document.getElementById("respGamba").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respGamba").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+
+function conferirHipopotamo() {
+    const respostaHipopotamo = document.getElementById("respostaHipopotamo").value;
+
+    if (respostaHipopotamo == "Hipopótamo") {
+        document.getElementById("respHipopotamo").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respHipopotamo").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirIca() {
+    const respostaIca = document.getElementById("respostaIca").value;
+
+    if (respostaIca == "Içá") {
+        document.getElementById("respIca").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respIca").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirJavali() {
+    const respostaJavali = document.getElementById("respostaJavali").value;
+
+    if (respostaJavali == "Javali") {
+        document.getElementById("respJavali").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respJavali").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirLeao() {
+    const respostaLeao = document.getElementById("respostaLeao").value;
+
+    if (respostaLeao == "Leão") {
+        document.getElementById("respLeao").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respLeao").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirMacaco() {
+    const respostaMacaco = document.getElementById("respostaMacaco").value;
+
+    if (respostaMacaco == "Macaco") {
+        document.getElementById("respMacaco").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respMacaco").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirNovilho() {
+    const respostaNovilho = document.getElementById("respostaNovilho").value;
+
+    if (respostaNovilho == "Novilho") {
+        document.getElementById("respNovilho").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respNovilho").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirOnca() {
+    const respostaOnca = document.getElementById("respostaOnca").value;
+
+    if (respostaOnca == "Onça") {
+        document.getElementById("respOnca").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respOnca").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirPaca() {
+    const respostaPaca = document.getElementById("respostaPaca").value;
+
+    if (respostaPaca == "Paca") {
+        document.getElementById("respPaca").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respPaca").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirQuati() {
+    const respostaQuati = document.getElementById("respostaQuati").value;
+
+    if (respostaQuati == "Quati") {
+        document.getElementById("respQuati").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respQuati").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirRato() {
+    const respostaRato = document.getElementById("respostaRato").value;
+
+    if (respostaRato == "Rato") {
+        document.getElementById("respRato").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respRato").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirSapo() {
+    const respostaSapo = document.getElementById("respostaSapo").value;
+
+    if (respostaSapo == "Sapo") {
+        document.getElementById("respSapo").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respSapo").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirTatu() {
+    const respostaTatu = document.getElementById("respostaTatu").value;
+
+    if (respostaTatu == "Tatu") {
+        document.getElementById("respTatu").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respTatu").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirUrubu() {
+    const respostaUrubu = document.getElementById("respostaUrubu").value;
+
+    if (respostaUrubu == "Urubu") {
+        document.getElementById("respUrubu").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respUrubu").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirVaca() {
+    const respostaVaca = document.getElementById("respostaVaca").value;
+
+    if (respostaVaca == "Vaca") {
+        document.getElementById("respVaca").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respVaca").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+function conferirZebra() {
+    const respostaZebra = document.getElementById("respostaZebra").value;
+
+    if (respostaZebra == "Zebra") {
+        document.getElementById("respZebra").innerHTML = "Parabéns!";
+    }
+    else {
+        document.getElementById("respZebra").innerHTML = "Resposta Errada! Tente novamente";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
